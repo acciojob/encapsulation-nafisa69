9d914031a2b0f3b50d8f45a57814c0d9ca345135
Nafisa <nafisa@gmail.com>
2023-10-17 16:41:26 +0530

    new obj added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
+    RWOnly obj= new RWOnly();
+    obj.name="Nafisa";
+    System.out.println(obj.name);
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,6 @@
 package com.driver;
 
 public class RWOnly {
-    
+    String name;
+
 }
